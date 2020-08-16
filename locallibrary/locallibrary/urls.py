0962a1f9c9f75a9bb087d3290ce4ad309a3d3a86
zampolitx <zampolitx_85@mail.ru>
2020-08-16 21:34:30 +0900

    второй коммит 16.08.2020(Добавлено приложение catalog)

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -19,3 +19,9 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# Используйте include() чтобы добавлять URL из каталога приложения
+from django.urls import include
+from django.urls import path
+urlpatterns += [
+     path('catalog/', include('catalog.urls')),
+]
